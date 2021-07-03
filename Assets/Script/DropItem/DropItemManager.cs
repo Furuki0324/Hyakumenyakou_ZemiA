975a210b630a5c9dc5c19872b9767c2ad311c21b
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2021-07-04 00:01:33 +0900

    Add item obtain system
    Addition:
    Enemy drops some item.
    And the dropped item will be collected when player take that.

---
+++ Assets/Script/DropItem/DropItemManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemManager : MonoBehaviour
+{
+    
+
+    private static int eyeElements = 0, earElements = 0, mouseElements = 0;
+
+    public static void ObtainItem(string type)
+    {
+        switch (type)
+        {
+            case "EyeElement":
+                eyeElements++;
+                break;
+
+            case "EarElement":
+                earElements++;
+                break;
+
+            case "MouseElement":
+                mouseElements++;
+                break;
+
+            default:
+                Debug.LogWarning("You tried to obtain an item with an error code.");
+                break;
+        }
+
+        Debug.Log("EyeElements: " + eyeElements + " EarElements: " + earElements + " MouseElements: " + mouseElements);
+    }
+}
