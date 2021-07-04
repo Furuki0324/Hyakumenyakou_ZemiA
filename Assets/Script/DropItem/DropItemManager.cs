b6daa843c3879a7b2069019b251c8571cda762f5
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2021-07-05 02:40:57 +0900

    Update DropItemManager

--- Assets/Script/DropItem/DropItemManager.cs
+++ Assets/Script/DropItem/DropItemManager.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DropItemManager : MonoBehaviour
 {
-    
+    //Instance prototype
+    private static DropItemManager thisInstance;
+
+
+    [Header("UI Text")]
+    public Text eyeText;
+    public Text earText;
+    public Text mouseText;
 
     private static int eyeElements = 0, earElements = 0, mouseElements = 0;
 
+
+    private void Awake()
+    {
+        thisInstance = this;
+        thisInstance.RefleshTexts();
+    }
+
     public static void ObtainItem(string type)
     {
+
         switch (type)
         {
             case "EyeElement":
@@ -29,6 +45,15 @@ public class DropItemManager : MonoBehaviour
                 break;
         }
 
+        thisInstance.RefleshTexts();
+
         Debug.Log("EyeElements: " + eyeElements + " EarElements: " + earElements + " MouseElements: " + mouseElements);
+    }
+
+    private void RefleshTexts()
+    {
+        eyeText.text = eyeElements.ToString();
+        earText.text = earElements.ToString();
+        mouseText.text = mouseElements.ToString();
     }
 }
