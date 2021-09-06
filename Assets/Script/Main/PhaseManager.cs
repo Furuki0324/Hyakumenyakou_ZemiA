cfaffea93bb8356cda15e2a2e40bf2f9dda52872
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2021-09-07 01:17:36 +0900

    add timer

--- Assets/Script/Main/PhaseManager.cs
+++ Assets/Script/Main/PhaseManager.cs
@@ -1,23 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhaseManager : MonoBehaviour
 {
+    [Header("Timer")]
+    [Tooltip("制限時間 (単位:秒)")]
+    public float time;
+    private int time_;
+    private int min;
+    private int second;
+    
+    [Header("UI")]
+    public Text text;
+
     public static int phaseNumber = 1;
 
     public static List<EnemyCtrl> enemies = new List<EnemyCtrl>();
+
+    private void Start()
+    {
+        text.text = phaseNumber.ToString();
+    }
+
+    private void Update()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        
+        time_ = (int)time;
+        min = time_ / 60;
+        second = time_ % 60;
+        text.text = "Time " + min.ToString("00") + ":" + second.ToString("00");
+    }
 
     /// <summary>
     /// <para>フェーズを次の段階へシフトします。</para>
     /// </summary>
     public static void PhaseShift()
     {
+        phaseNumber++;
+        
         EnemySpawnManager.Singleton.SpawnEnemy();
 
         phaseNumber++;
         Debug.Log("Phase: " + phaseNumber);
     }
+
 
 
     /// <summary>
