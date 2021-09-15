f6debcec73deb669621e110ac61a4004330fb6e2
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2021-09-15 16:58:52 +0900

    change physics settings

---
+++ Assets/Script/Player/PlayerRepairPartsScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRepairPartsScript : MonoBehaviour
+{
+    //Private
+    [ReadOnly, SerializeField]private List<FacePartsBaseScript> partsList = new List<FacePartsBaseScript>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FacePartsBaseScript baseScript = collision.gameObject.GetComponent<FacePartsBaseScript>();
+        if (baseScript)
+        {
+            partsList.Add(baseScript);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FacePartsBaseScript baseScript = collision.gameObject.GetComponent<FacePartsBaseScript>();
+        if (baseScript)
+        {
+            partsList.Remove(baseScript);
+        }
+    }
+
+}
