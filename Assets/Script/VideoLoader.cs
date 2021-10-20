1e09d4e90def52172f6a2be3c36e9dc376ffd258
Furuki0324 <im0176iv@ed.ritsumei.ac.jp>
2021-10-21 01:53:55 +0900

    WebGL FX

---
+++ Assets/Script/VideoLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoLoader : MonoBehaviour
+{
+    [SerializeField] private string relativePath;
+
+#if UNITY_WEBGL
+    private void Start()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.source = VideoSource.Url;
+        player.url = Application.streamingAssetsPath + "/" + relativePath;
+        player.prepareCompleted += PrepareCompleted;
+        player.Prepare();
+    }
+
+    private void PrepareCompleted(VideoPlayer vp)
+    {
+        vp.prepareCompleted -= PrepareCompleted;
+        //vp.Play();
+    }
+#endif
+
+}
